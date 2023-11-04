0009d891f6b73ce0d86cf4da5553b68fc2be880b
mate329 <mate32@me.com>
2023-11-04 13:20:24 +0100

    register

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -4,7 +4,6 @@ import jwt from 'jsonwebtoken'
 import { Request, Response } from 'express';
 import { ScanCommand, PutCommand } from "@aws-sdk/lib-dynamodb";
 import { DynamoDBClient, GetItemCommand } from "@aws-sdk/client-dynamodb";
-import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3"
 import { v4 as uuidv4 } from 'uuid';
 import { fromNodeProviderChain } from "@aws-sdk/credential-providers";
 
@@ -53,3 +52,46 @@ export const loginController = async (req: Request, res: Response) => {
       return res.status(500).json({ message: 'Error logging in user' });
     }
 };
+
+export const registerController = async (req: Request, res: Response) => {
+    try {
+        const { email, password } = req.body;
+  
+        if (!email || !password) {
+          return res.status(400).json({ message: 'Email, password and role are required' });
+        }
+  
+        // Check if user already exists
+        const input = {
+          TableName: tableName,
+          FilterExpression: 'email = :email',
+          ExpressionAttributeValues: { ':email': email }
+        }
+        const scancommand = new ScanCommand(input)
+        const existingUser = await dynamodb.send(scancommand)
+  
+        if (existingUser.Items && existingUser.Items.length > 0) {
+          return res.status(400).json({ message: 'User already exists' });
+        }
+  
+        // Create new user with unique id
+        const uuid = uuidv4();
+        // Hash password for security reasons
+        const saltRounds = 10
+        const hashedPassword = await bcrypt.hash(password, saltRounds);
+        const user = { uuid, email, password: hashedPassword };
+        
+        const putOperationInput = {
+          TableName: 'users',
+          Item: user
+        }
+        const putOperationCommand = new PutCommand(putOperationInput)
+        await dynamodb.send(putOperationCommand)
+    
+        // Return success response
+        return res.status(201).json({ message: 'User created successfully' });
+      } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: 'Error creating user' });
+      }
+  };
