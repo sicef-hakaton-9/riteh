121f6794b38ab80fcc471303ee853bf8d165d471
mate329 <mate32@me.com>
2023-11-04 14:53:51 +0100

    tablename

--- server/src/controllers/ticket.controller.ts
+++ server/src/controllers/ticket.controller.ts
@@ -5,7 +5,6 @@ import { ScanCommand, PutCommand } from "@aws-sdk/lib-dynamodb";
 import { DynamoDBClient, GetItemCommand } from "@aws-sdk/client-dynamodb";
 import { v4 as uuidv4 } from 'uuid';
 import { fromNodeProviderChain } from "@aws-sdk/credential-providers";
-import { table } from 'console';
 
 dotenv.config()
 
@@ -43,43 +42,6 @@ export const createTicket = async (req: Request, res: Response) => {
         console.log(`An error has occured during the ticket creation ${error}`);
         return res.status(500).json({ message: 'Error creating ticket' });
     }
-    //     if (!email || !password) {
-    //       return res.status(400).json({ message: 'Email, password and role are required' });
-    //     }
-
-    //     // Check if user already exists
-    //     const input = {
-    //       TableName: tableName,
-    //       FilterExpression: 'email = :email',
-    //       ExpressionAttributeValues: { ':email': email }
-    //     }
-    //     const scancommand = new ScanCommand(input)
-    //     const existingUser = await dynamodb.send(scancommand)
-
-    //     if (existingUser.Items && existingUser.Items.length > 0) {
-    //       return res.status(400).json({ message: 'User already exists' });
-    //     }
-
-    //     // Create new user with unique id
-    //     const uuid = uuidv4();
-    //     // Hash password for security reasons
-    //     const saltRounds = 10
-    //     const hashedPassword = await bcrypt.hash(password, saltRounds);
-    //     const user = { uuid, email, password: hashedPassword };
-        
-    //     const putOperationInput = {
-    //       TableName: 'users',
-    //       Item: user
-    //     }
-    //     const putOperationCommand = new PutCommand(putOperationInput)
-    //     await dynamodb.send(putOperationCommand)
-
-    //     // Return success response
-    //     return res.status(201).json({ message: 'User created successfully' });
-    //   } catch (error) {
-    //     console.error(error);
-    //     return res.status(500).json({ message: 'Error creating user' });
-    //   }
 };
 
 export const getTicketById = async (req: Request, res: Response) => {
