3fd72e6b02aa54b9eb098c5487399c92a506eb87
mate329 <mate32@me.com>
2023-11-05 06:35:11 +0100

    protecting endpoint

--- server/src/routes/chat.routes.ts
+++ server/src/routes/chat.routes.ts
@@ -1,7 +1,9 @@
 import express from 'express';
 import { getAllOpenChats } from '../controllers/chat.controller';
+import checkIfUserIsBusiness from '../middleware/checkIfUserIsBusiness'
+import checkIfUserCanAccess from '../middleware/checkIfUserCanAccess';
 const router = express.Router();
 
-router.get('/get-open-chats', getAllOpenChats);
+router.get('/get-open-chats', checkIfUserCanAccess, checkIfUserIsBusiness, getAllOpenChats);
 
 export default router;
