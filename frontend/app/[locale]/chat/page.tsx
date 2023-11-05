c58108bd0f0d268ab595f0c80c1407184be0e282
Antonia Grabar <antonia.grabar@gmail.com>
2023-11-05 10:25:43 +0100

    chat

--- frontend/app/[locale]/chat/page.tsx
+++ frontend/app/[locale]/chat/page.tsx
@@ -4,14 +4,19 @@ import { io } from "socket.io-client";
 import { useState } from "react";
 import ChatPage from "@/components/chatv2/chat";
 import { useSession } from "next-auth/react";
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
 import Endpoints from "@/constants/enums/Endpoints";
+import { useEffect } from "react";
+
+const generateUUID = () => {
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
+    const r = (Math.random() * 16) | 0;
+    const v = c === "x" ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
+};
 
 export default function Home() {
-  const [showChat, setShowChat] = useState(false);
-  const [userName, setUserName] = useState("");
-  const [roomId, setroomId] = useState("");
+  const [roomId, setRoomId] = useState("");
 
   const session = useSession();
   const token = session?.data?.accessToken;
@@ -26,41 +31,22 @@ export default function Home() {
   );
 
   const handleJoin = () => {
-    if (userName !== "" && roomId !== "") {
-      // console.log(userName, "userName", roomId, "roomId");
-      socket.emit("join_room", roomId);
-      setShowChat(true);
-    } else {
-      alert("Please fill in Username and Room Id");
-    }
+    const room = `cistoca-123e4567-e89b-12d3-a456-426614174000`;
+    socket.emit("join_room", room);
+    setRoomId(room);
   };
 
+  useEffect(() => {
+    handleJoin();
+  }, []);
+
   return (
-    <div className="flex flex-col justify-center items-center h-screen">
-      <div
-        className={
-          "w-[400px] flex flex-col gap-4 p-6 border border-gray-300 rounded-lg shadow-md"
-        }
-        style={{ display: showChat ? "none" : "" }}
-      >
-        <h1>Enter your info</h1>
-        <Input
-          type="text"
-          placeholder="Username"
-          onChange={(e) => setUserName(e.target.value)}
-        />
-        <Input
-          type="text"
-          placeholder="room id"
-          onChange={(e) => setroomId(e.target.value)}
-        />
-        <Button className="w-full mt-4" onClick={() => handleJoin()}>
-          Join
-        </Button>
-      </div>
-      <div style={{ display: !showChat ? "none" : "" }}>
-        <ChatPage socket={socket} roomId={roomId} username={userName} />
-      </div>
-    </div>
+    <>
+      <ChatPage
+        socket={socket}
+        roomId={roomId}
+        username={session.data?.user.email}
+      />
+    </>
   );
 }
