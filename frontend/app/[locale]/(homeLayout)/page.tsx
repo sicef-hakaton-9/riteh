9bc0cb3351f464aa249f10a60a66f745a585f61b
Antonia Grabar <antonia.grabar@gmail.com>
2023-11-05 04:33:46 +0100

    mock news

--- frontend/app/[locale]/(homeLayout)/page.tsx
+++ frontend/app/[locale]/(homeLayout)/page.tsx
@@ -2,6 +2,27 @@ import NewsCard from "@/components/newsCard";
 import WeatherCard from "@/components/weatherCard";
 import { getForecast, getWeather } from "@/services/weather";
 
+const news = [
+  {
+    description:
+      "The city of Rijeka is proud to announce a concert by the Symphony Orchestra, which will take place in the prestigious concert hall 'Zajc'. Classical music lovers will enjoy the performances of famous composers such as Beethoven, Mozart and Chopin. The conductor of the evening will be maestro Ivan Repušić, known for his passion and energy with which he brings every note to life. Visitors can expect an evening filled with the harmony and beauty of classical music that will echo through the walls of Rijeka.",
+    image: "/news/orchestra.png",
+    title: "Rijeka Symphony Orchestra Concert"
+  },
+  {
+    description:
+      "This weekend, culinary enthusiasts will have the opportunity to learn the secrets of Rijeka cuisine at a series of workshops led by chef Dario. A great opportunity for all gourmets to take a peek at the wealth of local flavors and learn how to prepare authentic Rijeka delicacies such as šurlica and brodet. The workshops will will be held in the old city center, offering participants not only a culinary, but also a cultural experience. Participants will cook, taste and, most importantly, enjoy socializing.",
+    image: "/news/workshop-cuisine.jpg",
+    title: "Workshops of Traditional Rijeka Cuisine"
+  },
+  {
+    description:
+      "Due to sudden works on road rehabilitation, there has been a major traffic jam on the Krčki Most, one of the key thoroughfares of the city of Rijeka. Residents are asked for patience while the emergency repairs take place. The works are expected to last throughout the day, and the traffic jam could affect the evening rush hour. Drivers are encouraged to use alternative routes to avoid congestion. The City of Rijeka apologizes to citizens for the inconvenience and thanks for your understanding.",
+    image: "/news/krk.jpg",
+    title: "Huge traffic jam on the Krčki Most"
+  }
+];
+
 export default async function Index() {
   const weather = await getWeather("Niš");
   const forecast = await getForecast("Niš", 3);
@@ -22,9 +43,15 @@ export default async function Index() {
           <p className="text-sm">--View more</p>
         </div>
         <div className="flex gap-2 justify-around md:flex-row flex-col">
-          <NewsCard />
-          <NewsCard />
-          <NewsCard />
+          {news.map((item, index) => (
+            <div key={index}>
+              <NewsCard
+                description={item.description}
+                title={item.title}
+                image={item.image}
+              />
+            </div>
+          ))}
         </div>
       </div>
     </>
--- frontend/components/newsCard/index.tsx
+++ frontend/components/newsCard/index.tsx
@@ -1,22 +1,31 @@
 import Image from "next/image";
 import { Card } from "../ui/card";
 
-export default function NewsCard() {
+export default function NewsCard({
+  description,
+  image,
+  title
+}: {
+  title: string;
+  description: string;
+  image: string;
+}) {
   return (
     <>
       <Card className="w-full h-full max-w-[400px] max-h-[300px] relative">
         <div className="w-full h-[150px] relative">
           <Image
-            src={
+            /*   src={
               "https://www.shutterstock.com/image-photo/cars-city-traffic-jams-stream-260nw-185515808.jpg"
-            }
+            } */
+            src={image}
             alt="article thumbnail"
             fill
             className="rounded-t-md object-cover"
           />
         </div>
         <div className="h-[150px] p-3 overflow-hidden text-ellipsis">
-          <h2 className="text-2xl font-bold line-clamp-2">
+          {/*   <h2 className="text-2xl font-bold line-clamp-2">
             --Heavy traffic at interstate
           </h2>
           <p className="text-sm text-gray-500 line-clamp-3">
@@ -25,7 +34,9 @@ export default function NewsCard() {
             the interstate. Reportedly there has been an accident involving two
             cars.People are reporting long lines at the interstate. Reportedly there
             has been an accident involving two cars.
-          </p>
+          </p> */}
+          <h2 className="text-2xl font-bold line-clamp-2">{title}</h2>
+          <p className="text-sm text-gray-500 line-clamp-3">{description}</p>
         </div>
       </Card>
     </>
